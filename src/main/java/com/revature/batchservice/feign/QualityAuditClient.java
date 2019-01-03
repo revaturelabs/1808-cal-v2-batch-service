239e2b45cc0dd2615aeb91da6585e2e2279b0792
Ian Barney <ianbarney23@gmail.com>
2019-01-03 06:21:27 -0500

    added feign client to communicate with audit service when week is added

---
+++ src/main/java/com/revature/batchservice/feign/QualityAuditClient.java
@@ -0,0 +1,35 @@
+package com.revature.batchservice.feign;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.revature.batchservice.entity.BatchEntity;
+
+/**
+ * A FeignClient for sending HTTP Requests to the QualityAudit Microservice.
+ * 
+ * Note that the url and endpoints are hardcoded.
+ * 
+ * @author Ian Barney
+ *
+ */
+@FeignClient(name = "audit-service", url = "localhost:9075/")
+public interface QualityAuditClient {
+	/**
+	 * Sends a HTTP Get request to the getLocationById endpoint in the
+	 * LocationService Controller. Uses the given id to specify which Location to
+	 * get.
+	 * 
+	 * @param locationId
+	 *            The Location id to search for.
+	 * @return A BatchEntity<String> from the LocationService. Should contain a
+	 *         Location address within its body of the form (LocationId, Location
+	 *         Name, Street Address City State ZipCode)
+	 */
+	@RequestMapping(method = RequestMethod.POST, value = "all/location/")
+	public ResponseEntity sendBatch(@RequestBody BatchEntity be);
+
+}
