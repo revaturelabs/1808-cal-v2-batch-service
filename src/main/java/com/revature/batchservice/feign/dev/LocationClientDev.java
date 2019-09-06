0a601c1a4276840d4074641f4eae7870a9f633d6
RevatureGentry <william.gentry@revature.com>
2019-09-06 16:01:19 -0400

    Named LocationServiceDev Feign Client

--- src/main/java/com/revature/batchservice/feign/dev/LocationClientDev.java
+++ src/main/java/com/revature/batchservice/feign/dev/LocationClientDev.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Profile;
  * @author William Gentry
  * 8/21/2019
  */
-@Profile("dev && !prod")
-@FeignClient(url = "http://location.caliber-2-dev")
+@Profile("dev")
+@FeignClient(name="location-service", url = "http://location.caliber-2-dev")
 public interface LocationClientDev extends LocationClient {
 }
