c31aba3d3f46a5f89e28293aabef66525e3f8496
Quntin Donnelly <quintin.donnelly@revature.com>
2019-12-20 16:23:51 -0500

    Updated some of the associate service
    pushing to move comps

--- src/main/java/com/revaturelabs/caliber/batch/AssociateService.java
+++ src/main/java/com/revaturelabs/caliber/batch/AssociateService.java
@@ -3,17 +3,18 @@ package com.revaturelabs.caliber.batch;
 import org.springframework.security.access.AccessDeniedException;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AssociateService {
   List<Associate> getAll() throws AccessDeniedException;
 
-  Associate getById(String id);
+  Optional<Associate> getById(String id);
 
-  Associate create(Associate batch);
+  Associate create(Associate associate);
 
-  Associate update(Associate category);
+  Associate update(Associate associate);
 
-  Associate createOrUpdate(Associate category);
+  Associate createOrUpdate(Associate associate);
 
   void delete(String id);
 }
--- src/main/java/com/revaturelabs/caliber/batch/AssociateServiceImp.java
+++ src/main/java/com/revaturelabs/caliber/batch/AssociateServiceImp.java
@@ -1,10 +1,15 @@
 package com.revaturelabs.caliber.batch;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class AssociateServiceImp implements AssociateService {
@@ -17,13 +22,13 @@ public class AssociateServiceImp implements AssociateService {
   }
 
   @Override
-  public Associate getById(String id) {
-    return null;
+  public Optional<Associate> getById(String id) {
+    return associateRepository.findById(id);
   }
 
   @Override
-  public Associate create(Associate batch) {
-    return null;
+  public Associate create(Associate associate) {
+    return associateRepository.save(associate);
   }
 
   @Override
